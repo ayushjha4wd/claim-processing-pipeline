f84247f04e5dd78126908bc4deba032bee3b64c6
ayushjha4wd <137145559+ayushjha4wd@users.noreply.github.com>
2026-04-16 13:06:57 +0530

    Update pdf_service.py

--- app/services/pdf_service.py
+++ app/services/pdf_service.py
@@ -1,12 +1,17 @@
 from pdf2image import convert_from_path
-import pytesseract
+import easyocr
+import numpy as np
+
+reader = easyocr.Reader(['en'])
 
 def extract_pages(pdf_path):
     images = convert_from_path(pdf_path)
     pages = []
 
     for img in images:
-        text = pytesseract.image_to_string(img)
+        img_np = np.array(img)
+        result = reader.readtext(img_np, detail=0)
+        text = " ".join(result)
         pages.append(text)
 
     return pages
