6fe50192f4f11f516600f60b7c0528d608a43f2a
ayushjha4wd <137145559+ayushjha4wd@users.noreply.github.com>
2026-04-16 12:39:03 +0530

    Update file_handler.py

--- app/utils/file_handler.py
+++ app/utils/file_handler.py
@@ -3,8 +3,9 @@ import os
 UPLOAD_DIR = "uploads"
 
 def save_file(file):
-    os.makedirs(UPLOAD_DIR, exist_ok=True)
-    
+    if not os.path.isdir(UPLOAD_DIR):
+        os.makedirs(UPLOAD_DIR, exist_ok=True)
+
     file_path = os.path.join(UPLOAD_DIR, file.filename)
 
     with open(file_path, "wb") as f:
