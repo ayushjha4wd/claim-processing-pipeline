f0808fa3bd74037a0d2f6aee13fd423aa8eb679c
ayushjha4wd <137145559+ayushjha4wd@users.noreply.github.com>
2026-04-16 14:30:34 +0530

    Update pdf_service.py

--- app/services/pdf_service.py
+++ app/services/pdf_service.py
@@ -1,17 +1,11 @@
-from pdf2image import convert_from_path
-import easyocr
-import numpy as np
-
-reader = easyocr.Reader(['en'], gpu=False)
+from PyPDF2 import PdfReader
 
 def extract_pages(pdf_path):
-    images = convert_from_path(pdf_path)
+    reader = PdfReader(pdf_path)
     pages = []
 
-    for img in images:
-        img_np = np.array(img)
-        result = reader.readtext(img_np, detail=0)
-        text = " ".join(result)
+    for page in reader.pages:
+        text = page.extract_text() or ""
         pages.append(text)
 
     return pages
